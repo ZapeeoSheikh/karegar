2c669046a0ce6c6ac8c5d7f6c2c828816d138bbe
BilalKashif <bilalkashif467@gmail.com>
2023-05-06 21:25:22 +0500

    merg both branches

--- lib/view/addLocation_view.dart
+++ lib/view/addLocation_view.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:softec/view/home_page.dart';
 import '../utils/r_colors.dart';
 import 'chooseRole_view.dart';
