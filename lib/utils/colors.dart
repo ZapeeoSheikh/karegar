1e6a7833e2206b2bd54593e8c649930e382cab91
hanan <hanan@gmail.com>
2023-05-07 03:06:50 +0500

    bottom nav post

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -9,8 +9,15 @@ class KColors {
   static const kSecondary = Color(0xffe7ffff);
   static const kWhite = Colors.white;
   static const kBlack = Color(0xff000000);
+  // static const kGrey = Color(0xff838383);
+  static const kGrey2 = Color(0xff7d7d7d);
   static const kGrey = Color(0xff838383);
-  static const kRed = Color(0xfff00c0c);
+  static const kGrey1 = Color(0xff777777);
+  static const kGrey3 = Color(0xffc1c1c1);
+  static const kLightGrey = Color(0xfff6f6f6);
+  static const kYellow = Color(0xfff2ba27);
+  static const kRed = Color(0xffBB3F3F);
+  // static const kRed = Color(0xfff00c0c);
   static const kGreen = Color(0xff7cc32c);
   static const noColor = Colors.transparent;
 
