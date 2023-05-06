8c384fee431a6f3a5736785e600c26d33b588274
ZapeeoSheikh <muhammadrmz871@gmail.com>
2023-05-07 03:54:00 +0500

    homepage, add skills

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -17,6 +18,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int userType = 0;
 
+  List<String> items = [
+    "Carpenter", "Driver", "Electrician", "Painter", "Plumber", "Web Developer","Mechanices","Ironing","Delivery","Cleaning","Cooking","Planner","Gardener","Photographer","Tailor",
+  ];
+  String? selectedValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,31 +34,130 @@ class _HomePageState extends State<HomePage> {
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: userType == 1
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image(image: AssetImage("assets/Vector/unavailable.png")),
-                    Text(
-                      "You can not see the job posting",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 5.h,
-                    ),
-                    Text(
-                      "Join as a trade person",
-                      style: TextStyle(
-                        color: MyColor.mainColor1,
-                        fontSize: 16.sp,
+              ? Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: MediaQuery.of(context).size.width/3,),
+                      Image(image: AssetImage("assets/Vector/unavailable.png")),
+                      Text(
+                        "You can not see the job posting",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.bold),
                       ),
-                    ),
-                  ],
-                )
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      Text(
+                        "Join as a trade person",
+                        style: TextStyle(
+                          color: MyColor.mainColor1,
+                          fontSize: 16.sp,
+                        ),
+                      ),
+                    ],
+                  ),
+              )
               : Column(
                   children: [
+                    Row(
+                      children: [
+                        DropdownButtonHideUnderline(
+                          child: DropdownButton2(
+                            isExpanded: true,
+                            hint: Row(
+                              children: [
+                                ImageIcon(
+                                  AssetImage(
+                                    "assets/icons/filter.png",
+                                  ),
+                                  color: MyColor.textColor1,
+                                ),
+                                SizedBox(
+                                  width: 4,
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    ' Select Item',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            items: items
+                                .map((item) => DropdownMenuItem<String>(
+                              value: item,
+                              child: Text(
+                                item,
+                                style: TextStyle(
+                                  fontSize: 14.sp,
+                                  // fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ))
+                                .toList(),
+                            value: selectedValue,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedValue = value as String;
+                              });
+                            },
+                            buttonStyleData: ButtonStyleData(
+                              height: 50,
+                              width: 160,
+                              padding: const EdgeInsets.only(left: 14, right: 14),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(14),
+                                border: Border.all(
+                                  color: Colors.black26,
+                                ),
+                                color: MyColor.mainColor2,
+                              ),
+                              elevation: 2,
+                            ),
+                            iconStyleData: IconStyleData(
+                              icon: Icon(
+                                Icons.arrow_forward_ios_outlined,
+                              ),
+                              iconSize: 14,
+                              iconEnabledColor: MyColor.mainColor1,
+                              iconDisabledColor: Colors.grey,
+                            ),
+                            dropdownStyleData: DropdownStyleData(
+                              maxHeight: 200,
+                              width: 200,
+                              padding: null,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(14),
+                                color: MyColor.mainColor2,
+                              ),
+                              elevation: 8,
+                              offset: const Offset(-20, 0),
+                              scrollbarTheme: ScrollbarThemeData(
+                                radius: const Radius.circular(40),
+                                thickness: MaterialStateProperty.all<double>(6),
+                                thumbVisibility: MaterialStateProperty.all<bool>(true),
+                              ),
+                            ),
+                            menuItemStyleData: const MenuItemStyleData(
+                              height: 40,
+                              padding: EdgeInsets.only(left: 14, right: 14),
+                            ),
+                          ),
+                        ),
+
+                      ],
+                    ),
+                    SizedBox(height: 10.h,),
                     for(int i = 0; i <= 20; i++)
                     Padding(
                       padding: const EdgeInsets.only(bottom: 10.0),
