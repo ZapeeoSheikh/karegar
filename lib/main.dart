350e50331004d1f28c72b2e13c19822f1988e2a7
BilalKashif <bilalkashif467@gmail.com>
2023-05-07 09:25:23 +0500

    portrait Restriction

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:softec/res/providers.dart';
@@ -13,7 +14,9 @@ void main() async {
   await Firebase.initializeApp(
     // options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+    SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((value) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
